cf0f1763deb5f8fab0e30dea2683fb7193f3e098
Shubhas <nimtsubash@gmail.com>
2021-07-01 21:59:42 +0545

    models and static configured

--- todoApi/todo/models.py
+++ todoApi/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import datetime
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Todo(models.Model):
+    id = models.IntegerField(primary_key=True, null=False, blank=False)
+    short_description = models.TextField(blank=False)
+    long_description = models.TextField()
+    allocated_time = models.TextField()
+    created_date = models.DateTimeField(default=datetime.datetime.now())
+    created_by = models.ForeignKey(User)
+    completed = models.BooleanField(default=False)
--- todoApi/todoApi/settings.py
+++ todoApi/todoApi/settings.py
@@ -120,8 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_cdn'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media_cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom setting goes here
+
+# Auth user
+AUTH_USER_MODEL = 'todo.User'
